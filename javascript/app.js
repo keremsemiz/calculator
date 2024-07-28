2708c2c6eb71c402207e290af39b2b7fd7841099
Kerem Semiz <kerox350@gmail.com>
2024-07-28 21:38:09 +0300

    JS Calculator work nce more

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,52 @@
-const inputs = document.querySelectorAll('input');
-const body = document.getElementById('body');
+import { setTheme, setInitialTheme } from './functions/themeSwitcher.js';
+import createButtons from './functions/createButtons.js';
 
+createButtons();
+setInitialTheme();
 
-function setTheme(theme) {
-    body.classList = theme;
-}
+import handleClick from './functions/handleClick.js';
+import handleKeys from './functions/handleKeys.js';
+const inputs = document.querySelectorAll('input[name="theme"]');
+const btnEls = document.querySelectorAll('.js-calc-btn');
+const firstGridBtn = document.querySelector('.js-calc-btn');
+const NUX = document.getElementById('grid-nux');
+const closeNUX = document.getElementById('close-nux-btn');
 
-function setInitialTheme() {
-    const darkInput = document.getElementById('dark');
-    const lightInput = document.getElementById('light');
-    const neonInput = document.getElementById('neon');
-    console.log(darkInput);
+let mouseDown = false;
 
-    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
-
-    if(userPrefersDark){
-        darkInput.setAttribute('checked', '');
-        setTheme('dark-theme');
-    } else if (userPrefersLight) {
-        lightInput.setAttribute('checked', '');
-        setTheme('light-theme');
-    } else {
-        console.log('no-preference');
+const showInstructions = () => {
+    if (!mouseDown) {
+        firstGridBtn.removeEventListener('focus', showInstructions);
+        NUX.classList.remove('hidden');
+        NUX.focus();
     }
 }
 
-setInitialTheme();
+const hideInstructions = () => {
+    NUX.classList.add('hidden');
+    firstGridBtn.focus();
+}
 
 inputs.forEach(input => {
     input.addEventListener('input', e => {
         setTheme(e.target.value);
     })
 })
+
+firstGridBtn.addEventListener('mousedown', () => {
+    mouseDown = true;
+})
+
+firstGridBtn.addEventListener('mouseup', () => {
+    mouseDown = false;
+})
+
+firstGridBtn.addEventListener('focus', showInstructions);
+
+closeNUX.addEventListener('click', hideInstructions);
+
+btnEls.forEach(button => {
+    button.addEventListener('click', handleClick);
+})
+
+document.addEventListener('keydown', handleKeys);
